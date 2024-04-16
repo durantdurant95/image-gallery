3574765673b80bdaa5ecbc29b94575fd661f279f
Alejandro Pérez Durán <aperez@datamart.cl>
2024-04-16 13:48:27 -0400

    Add top navigation to layout and image gallery to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,27 @@
+import Image from "next/image";
+
 export default function HomePage() {
+  const mockUrls = [
+    "https://utfs.io/f/bafea2fa-3073-4f34-94f0-c642d1a4a960-tcjfko.jpg",
+    "https://utfs.io/f/67ac8133-e37f-48f1-9628-406383bd456a-o6jcr9.jpg",
+    "https://utfs.io/f/3c55db43-1ffe-4411-ae29-89a3564795df-of8bho.jpg",
+    "https://utfs.io/f/81339ae7-7851-4f83-aa84-629929262e91-5kvpfo.jpg",
+  ];
+
+  const mockImages = mockUrls.map((url, index) => ({
+    id: index,
+    url,
+  }));
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <p>Hello (Image Gallery in progress)</p>
+    <main>
+      <div className="flex flex-wrap gap-4">
+        {mockImages.map((image) => (
+          <div key={image.id} className="relative h-24 w-48">
+            <Image src={image.url} alt="" fill />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
