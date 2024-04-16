d192b7378a675e2e35c09a55571fb6a8903c132b
Alejandro Pérez Durán <aperez@datamart.cl>
2024-04-16 14:50:45 -0400

    Refactor homepage to use dynamic images and update schema for image table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,37 +2,18 @@ import Image from "next/image";
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
-const mockUrls = [
-  "https://utfs.io/f/bafea2fa-3073-4f34-94f0-c642d1a4a960-tcjfko.jpg",
-  "https://utfs.io/f/67ac8133-e37f-48f1-9628-406383bd456a-o6jcr9.jpg",
-  "https://utfs.io/f/3c55db43-1ffe-4411-ae29-89a3564795df-of8bho.jpg",
-  "https://utfs.io/f/81339ae7-7851-4f83-aa84-629929262e91-5kvpfo.jpg",
-];
-
-const mockImages = mockUrls.map((url, index) => ({
-  id: index,
-  url,
-}));
 
 export default async function HomePage() {
-  const posts = await db.query.posts.findMany();
+  const images = await db.query.images.findMany();
 
   return (
     <main>
       <div className="flex flex-wrap gap-4">
-        {mockImages.map((image) => (
+        {images.map((image) => (
           <div key={image.id} className="relative h-24 w-48">
             <Image src={image.url} alt="" fill />
           </div>
         ))}
-      </div>
-      <div>
-        <h1 className="text-2xl font-bold">Posts</h1>
-        <ul>
-          {posts.map((post) => (
-            <li key={post.id}>{post.name}</li>
-          ))}
-        </ul>
       </div>
     </main>
   );
