7b4c066611f6128245c4263fe9ac7c5f6a156551
Alejandro Pérez Durán <aperez@datamart.cl>
2024-04-24 00:09:20 -0400

    Refactor FullPageImageView component to display uploader info and creation date

--- src/components/full-page-image-view.tsx
+++ src/components/full-page-image-view.tsx
@@ -1,13 +1,19 @@
+import { clerkClient } from "@clerk/nextjs/server";
 import { getImage } from "~/server/queries";
 export default async function FullPageImageView(props: { id: number }) {
+  // Get the image data from the server
   const image = await getImage(props.id);
+
+  // Call Clerk to get the user's info
+  const uploaderInfo = await clerkClient.users.getUser(image.userId);
 
   return (
     <div className="flex h-full items-center gap-8 p-4">
       <img src={image.url} alt={image.name} className="w-2/3 object-contain" />
       <div className="flex w-1/3 flex-col gap-4">
         <p>{image.name}</p>
-        <p>{image.url}</p>
+        <p>Uploaded by: {uploaderInfo.fullName}</p>
+        <p>Created on: {new Date(image.createdAt).toLocaleDateString()}</p>
       </div>
     </div>
   );
